8f711943f204791fa7e931e0c440fd868b8544d4
liamstead05 <000952741@mohawkcollege.ca>
2024-10-08 17:53:55 -0400

    Add content

--- interactivedrawing-stead-zoe-assignment2-truthfulcolours/Game.cs
+++ interactivedrawing-stead-zoe-assignment2-truthfulcolours/Game.cs
@@ -11,14 +11,19 @@ namespace Game10003
     public class Game
     {
         // Place your variables here:
-
+        Color circle = Random.Color();
+        Color square = Random.Color();
+        Color triangle = Random.Color();
+        Color hexagon = Random.Color();
+        bool mouseClick;
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
-
+            Window.SetTitle("truthful colours");
+            Window.SetSize(400, 400);
         }
 
         /// <summary>
@@ -26,6 +31,13 @@ namespace Game10003
         /// </summary>
         public void Update()
         {
+            mouseClick = Input.IsMouseButtonPressed(MouseInput.Left);
+            Draw.FillColor = circle;
+            Draw.Circle(100, 100, 75);
+            if (mouseClick==true)
+            {
+                circle = Random.Color();
+            }
 
         }
     }
