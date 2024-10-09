2769c21ba45466fec89890e993d145eb4d965d15
liamstead05 <000952741@mohawkcollege.ca>
2024-10-09 17:04:23 -0400

    Add box separators Change shape coordinates Add background

--- interactivedrawing-stead-zoe-assignment2-truthfulcolours/Game.cs
+++ interactivedrawing-stead-zoe-assignment2-truthfulcolours/Game.cs
@@ -11,10 +11,12 @@ namespace Game10003
     public class Game
     {
         // Place your variables here:
+        // Variables for the random colours of the four shapes
         Color circle = Random.Color();
         Color square = Random.Color();
         Color triangle = Random.Color();
         Color hexagon = Random.Color();
+        Color box = new Color(190, 190, 190);
         bool mouseClick;
 
         /// <summary>
@@ -22,6 +24,7 @@ namespace Game10003
         /// </summary>
         public void Setup()
         {
+            // Window size and title, which is the name of the application
             Window.SetTitle("truthful colours");
             Window.SetSize(400, 400);
         }
@@ -31,32 +34,48 @@ namespace Game10003
         /// </summary>
         public void Update()
         {
+            // Change background colour to selected background colour
+            Window.ClearBackground(Color.OffWhite);
+            
+            // Cal
             mouseClick = Input.IsMouseButtonPressed(MouseInput.Left);
             Draw.FillColor = circle;
-            Draw.Circle(100, 100, 75);
+            Draw.LineColor = Color.Black;
+            Draw.Circle(105, 105, 75);
             if (mouseClick==true)
             {
                 circle = Random.Color();
             }
             Draw.FillColor = square;
-            Draw.Square(225, 25, 150);
+            Draw.LineColor = Color.Black;
+            Draw.Square(220, 30, 150);
             if (mouseClick==true)
             {
                 square = Random.Color();
             }
             Draw.FillColor = triangle;
-            Draw.Triangle(100, 250, 25, 375, 175, 375);
+            Draw.LineColor = Color.Black;
+            Draw.Triangle(105, 240, 30, 365, 180, 365);
             if (mouseClick==true)
             {
                 triangle = Random.Color();
             }
             Draw.FillColor = hexagon;
-            Draw.Quad(225, 310, 265, 250, 335, 250, 375, 310);
-            Draw.Quad(225, 310, 265, 375, 335, 375, 375, 310);
+            Draw.LineColor = Color.Black;
+            Draw.Quad(220, 295, 260, 235, 330, 235, 370, 295);
+            Draw.Quad(220, 295, 260, 360, 330, 360, 370, 295);
             if (mouseClick==true)
             {
                 hexagon = Random.Color();
             }
+            Draw.FillColor = box;
+            Draw.LineColor = box;
+            Draw.Rectangle(0, 0, 400, 20);
+            Draw.Rectangle(0, 0, 20, 400);
+            Draw.Rectangle(0, 380, 400, 20);
+            Draw.Rectangle(380, 0, 20, 400);
+            Draw.Rectangle(190, 0, 20, 400);
+            Draw.Rectangle(0, 190, 400, 20);
         }
     }
 }
