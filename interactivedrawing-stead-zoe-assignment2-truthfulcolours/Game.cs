011ea2f566f76258bfb45ce10cb73432ba7006bc
liamstead05 <000952741@mohawkcollege.ca>
2024-10-09 15:03:29 -0400

    Add square Add triangle Add hexagon

--- interactivedrawing-stead-zoe-assignment2-truthfulcolours/Game.cs
+++ interactivedrawing-stead-zoe-assignment2-truthfulcolours/Game.cs
@@ -38,7 +38,25 @@ namespace Game10003
             {
                 circle = Random.Color();
             }
-
+            Draw.FillColor = square;
+            Draw.Square(225, 25, 150);
+            if (mouseClick==true)
+            {
+                square = Random.Color();
+            }
+            Draw.FillColor = triangle;
+            Draw.Triangle(100, 250, 25, 375, 175, 375);
+            if (mouseClick==true)
+            {
+                triangle = Random.Color();
+            }
+            Draw.FillColor = hexagon;
+            Draw.Quad(225, 310, 265, 250, 335, 250, 375, 310);
+            Draw.Quad(225, 310, 265, 375, 335, 375, 375, 310);
+            if (mouseClick==true)
+            {
+                hexagon = Random.Color();
+            }
         }
     }
 }
